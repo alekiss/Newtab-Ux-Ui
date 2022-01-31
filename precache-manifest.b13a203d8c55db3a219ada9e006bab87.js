5027e114e814810683c0d60c8bba18686e1cf225
Alekiss Melo <alekiss.melo@dcx.ufpb.br>
2022-01-31 20:12:55 -0300

    Updates

---
+++ precache-manifest.b13a203d8c55db3a219ada9e006bab87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4989fa844ac86a8840cb75ec6ff6221a",
+    "url": "/Newtab-Ux-Ui/index.html"
+  },
+  {
+    "revision": "24d5662f479ae6f1fcde",
+    "url": "/Newtab-Ux-Ui/static/css/main.a4ed7003.chunk.css"
+  },
+  {
+    "revision": "10ae0603604359519bfb",
+    "url": "/Newtab-Ux-Ui/static/js/2.50c414a1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Newtab-Ux-Ui/static/js/2.50c414a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d5662f479ae6f1fcde",
+    "url": "/Newtab-Ux-Ui/static/js/main.e3f720bb.chunk.js"
+  },
+  {
+    "revision": "1e63d6494976c5ffb6e3",
+    "url": "/Newtab-Ux-Ui/static/js/runtime-main.372afa67.js"
+  }
+]);
